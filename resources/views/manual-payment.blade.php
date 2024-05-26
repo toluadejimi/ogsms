2149a26dbbbd023325b16115ce4b12ecd689e1bd
Jimmy <toluadejimi@gmail.com>
2024-05-26 20:29:42 +0100

    pushhhhhh

--- resources/views/manual-payment.blade.php
+++ resources/views/manual-payment.blade.php
@@ -405,7 +405,7 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
-                        Copyright © 2018 Log Marketplace. All rights reserved. Dashboard by <a
+                        Copyright © 2018 OGSMSPOOL All rights reserved. Dashboard by <a
                             href="https://colorlib.com/wp/">Colorlib</a>.
                     </div>
                     <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
