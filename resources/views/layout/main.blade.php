4f2d026ba4d07d3261a7d3a48d66e8321e2d0364
Jimmy <toluadejimi@gmail.com>
2024-06-16 12:22:20 +0100

    pushhhhhh

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -167,6 +167,12 @@
 <script>main_layout_change('vertical');</script>
 
 
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+</body>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+
 <style>
     .float{
         position:fixed;
@@ -186,7 +192,289 @@
     .my-float{
         margin-top:16px;
     }
+
+
+    .twitter {
+        font: normal normal 10px Arial;
+        text-align: center;
+        color: #998578;
+        text-transform: uppercase;
+        letter-spacing: 3px;
+    }
+
+    .twitter {
+        color: #000000;
+        text-decoration: none;
+        display: block;
+        padding: 14px;
+        -webkit-transition: all .25s ease;
+        -moz-transition: all .25s ease;
+        -ms-transition: all .25s ease;
+        -o-transition: all .25s ease;
+        transition: all .25s ease;
+    }
+
+    .twitter :hover {
+        color: #FF7D6D;
+        text-decoration: none;
+    }
+
+    /* Floating Social Media Bar Style Starts Here */
+
+    .fl-fl {
+        background: #000000;
+        text-transform: uppercase;
+        letter-spacing: 3px;
+        padding: 4px;
+        width: 190px;
+        position: fixed;
+        right: -145px;
+        z-index: 1000;
+        font: normal normal 10px Arial;
+        -webkit-transition: all .25s ease;
+        -moz-transition: all .25s ease;
+        -ms-transition: all .25s ease;
+        -o-transition: all .25s ease;
+        transition: all .25s ease;
+    }
+
+    .s-fab {
+        font-size: 20px;
+        color: #fff;
+        padding: 10px 0;
+        width: 40px;
+        margin-left: 10px;
+    }
+
+    .fl-fl:hover {
+        right: 0;
+    }
+
+    .fl-fl a {
+        color: #fff !important;
+        text-decoration: none;
+        text-align: center;
+        line-height: 43px!important;
+        vertical-align: top!important;
+    }
+
+    .float-fb {
+        top: 160px;
+    }
+
+    .float-tw {
+        top: 215px;
+    }
+
+    .float-gp {
+        top: 270px;
+    }
+
+    .float-rs {
+        top: 325px;
+    }
+
+    .float-ig {
+        top: 380px;
+    }
+
+    .float-pn {
+        top: 435px;
+    }
+    /* Floating Social Media Bar Style Ends Here */
+
+
+    /* Follow us DropDown Starts */
+
+    .btn-clr {
+        /*   background: linear-gradient(90deg, rgba(2,0,36,0) 0%, rgba(10,221,8,0.9307073171065301) 0%, rgba(8,220,50,0.9251050762101716) 39%, rgba(6,218,99,0.9251050762101716) 73%, rgba(4,216,156,0.9335084375547094) 100%, rgba(0,212,255,0) 100%); */
+        color : #ffffff;
+        font : 900 16px/20px 'Lato' sans serif;
+    }
+
+    .btn-clr>button:hover{
+        background-color: #000000;
+        color : #0add08 ;
+    }
+
+    .dropdown-menu>li>a:hover,
+    .dropdown-menu>li>a:focus{
+        background-color: none!important;
+    }
+    .tb, .tb2, .tb3, .tb4{
+        display:flex;
+    }
+
+    .tb, .tb2,.tb3{
+        border-bottom: 3px solid white;
+    }
+
+    .sb {
+        padding:10px 0;
+        color:white
+    }
+
+    .sb-fb{
+        padding-right : 3px;
+    }
+    .tb-sb-fb{
+        background-color: #3b5998;
+        border-right: 3px solid white;
+    }
+    .sb-tw {
+        padding-right : 3px;
+    }
+    .tb-sb-tw{
+        background-color: #1da1f2;
+        border-right: 3px solid white;
+    }
+    .tb4-sb-rss{
+        background-color: #ee802f;
+        border-right: 3px solid white;
+    }
+    .sb-tg{
+        padding-right :2px;
+    }
+    .tb2-sb-tg{
+        background-color: #0088cc;
+    }
+
+    .sb-in{
+        padding-right :1.5px;
+    }
+    .tb-sb-in{
+        background-color: #0a66c2;
+    }
+    .tb2-sb-ig{
+        background: #f09433;
+        background: -moz-linear-gradient(45deg, #f09433 0%, #e6683c 25%, #dc2743 50%, #cc2366 75%, #bc1888 100%);
+        background: -webkit-linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%);
+        background: linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%);
+        filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#f09433', endColorstr='#bc1888',GradientType=1 );
+        border-right: 3px solid white;
+    }
+    .sb-tk{
+        padding-right : 0.5px;
+    }
+    .tb3-sb-tk{
+        background-color: #000;
+        border-right: 3px solid white;
+    }
+    .tb2-sb-yt{
+        background-color: #ff0000;
+        border-right: 3px solid white;
+    }
+
+    .tb3-sb-med{
+        background-color: #02b875;
+    }
+    .sb-whapp{
+        padding-right : 3.5px;
+    }
+    .tb3-sb-whapp{
+        background-color: #25d366;
+        border-right: 3px solid white;
+    }
+    /* DropDown Ends */
+
+    /* Tiktok Logo */
+    .tiktok{
+        position: relative;
+        width: 200px;
+        margin: 100px auto;
+    }
+
+    .logo {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 47px;
+        height: 218px;
+        z-index: 1;
+        background: #24f6f0;
+    }
+
+      .logo::after {
+          font-family:'FontAwesome';
+          content: "e07b";
+          position: absolute;
+          width: 140px;
+          height: 140px;
+          border: 40px solid #24f6f0;
+          border-right: 40px solid transparent;
+          border-top: 40px solid transparent;
+          border-left: 40px solid transparent;
+          top: -110px;
+          right: -183px;
+          border-radius: 100%;
+          transform: rotate(45deg);
+          z-index: -10;
+      }
+
+      .logo::before {
+          content: "e07b";
+          position: absolute;
+          width: 100px;
+          height: 100px;
+          border: 47px solid #24f6f0;
+          border-top: 47px solid transparent;
+          border-radius: 50%;
+          top: 121px;
+          left: -147px;
+          transform: rotate(45deg);
+      }
+
+    .logo:last-child {
+        left: 10px;
+        top: 10px;
+        background: #fe2d52;
+        z-index: 100;
+    }
+
+      .logo:last-child::before {
+          border: 47px solid #fe2d52;
+          border-top: 47px solid transparent;
+      }
+    .logo:last-child::after {
+        border: 40px solid #fe2d52;
+        border-right: 40px solid transparent;
+        border-top: 40px solid transparent;
+        border-left: 40px solid transparent;
+    }
+
+
+    .logo:last-child {
+        mix-blend-mode: lighten;
+    }
 </style>
+
+
+<div class="float-sm">
+    <div class="fl-fl float-fb">
+        <i class="fab fa-facebook-f s-fab"></i>
+        <a href="" target="_blank"> Like us!</a>
+    </div>
+    <div class="fl-fl float-tw">
+        <i class="fab fa-twitter s-fab"></i>
+        <a href="" target="_blank">Follow us!</a>
+    </div>
+    <div class="fl-fl float-gp">
+        <i class="fab fa-google-plus-g s-fab"></i>
+        <a href="" target="_blank">Recommend us!</a>
+    </div>
+    <div class="fl-fl float-rs">
+        <i class="fas fa-rss s-fab"></i>
+        <a href="" target="_blank">Follow via RSS</a>
+    </div>
+    <div class="fl-fl float-ig">
+        <i class="fab fa-instagram s-fab"></i>
+        <a href="" target="_blank">Follow us!</a>
+    </div>
+    <div class="fl-fl float-pn">
+        <i class="fab fa-pinterest-p s-fab"></i>
+        <a href="" target="_blank">Follow us!</a>
+    </div>
+</div>
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 <a href="https://api.whatsapp.com/send?phone=2349047123389" class="float" target="_blank">
