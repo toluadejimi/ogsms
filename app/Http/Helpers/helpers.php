2499d5acb237ee639e0fd4e3454b53536ec2672a
Jimmy <toluadejimi@gmail.com>
2024-07-13 10:13:55 +0100

    pushhhhhh

--- app/Http/Helpers/helpers.php
+++ app/Http/Helpers/helpers.php
@@ -4,8 +4,11 @@ use App\Constants\Status;
 use App\Models\Extension;
 use App\Models\Verification;
 use App\Lib\GoogleAuthenticator;
+use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Auth;
-
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 
 
 function resolve_complete($order_id)
@@ -349,86 +352,67 @@ function check_sms($orderID){
 
 
 function get_world_countries(){
+
     $key = env('WKEY');
-    $curl = curl_init();
-    curl_setopt_array($curl, array(
-        CURLOPT_URL => "https://api.smspool.net/country/retrieve_all?key=$key",
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => '',
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => 'POST',
-        CURLOPT_HTTPHEADER => array(
-            'Content-Type: application/json',
-            'Accept: application/json',
-        ),
-    ));
 
-    $var = curl_exec($curl);
-    curl_close($curl);
-    $var = json_decode($var);
-    $countries = $var ?? null;
+    $countries = Cache::remember('smspool_countries', 3600, function () use ($key) {
+        Log::info('Requesting countriees from SMS Pool API', ['key' => $key]);
 
+        $response = Http::withHeaders([
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json',
+        ])->timeout(50)
+            ->post("https://api.smspool.net/country/retrieve_all", [
+                'key' => $key,
+            ]);
 
+        Log::info('Response received from SMS Pool API', ['response' => $response->body()]);
 
+        if ($response->successful()) {
+            return $response->json() ?? null;
+        }
 
-    // $history = [];
-    // foreach ($countries as $key => $value) {
-    //     $history[] = array(
-    //         "country_id" => $value->ID,
-    //         "name" => $value->name,
-    //         "short_name" => $value->short_name,
-
-    //     );
-    // }
-
-    // $rr =  DB::table('countries')->insert($history);
-
-
-
-
-    if ($var == null) {
-        $coiuntries = null;
-    }
-
+        Log::error('API call failed', ['response' => $response->body()]);
+        return null;
+    });
 
     return $countries;
+
+
+
 }
 
 
 function get_world_services(){
 
+
     $key = env('WKEY');
 
-    $curl = curl_init();
-    curl_setopt_array($curl, array(
-        CURLOPT_URL => "https://api.smspool.net/service/retrieve_all?key=$key",
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_ENCODING => '',
-        CURLOPT_MAXREDIRS => 10,
-        CURLOPT_TIMEOUT => 0,
-        CURLOPT_FOLLOWLOCATION => true,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => 'POST',
-        CURLOPT_HTTPHEADER => array(
-            'Content-Type: application/json',
-            'Accept: application/json',
-        ),
-    ));
+    $services = Cache::remember('smspool_countries', 3600, function () use ($key) {
+        Log::info('Requesting countriees from SMS Pool API', ['key' => $key]);
 
-    $var = curl_exec($curl);
-    curl_close($curl);
-    $var = json_decode($var);
-    $services = $var ?? null;
+        $response = Http::withHeaders([
+            'Content-Type' => 'application/json',
+            'Accept' => 'application/json',
+        ])->timeout(50)
+            ->post("https://api.smspool.net/service/retrieve_all", [
+                'key' => $key,
+            ]);
 
+        Log::info('Response received from SMS Pool API', ['response' => $response->body()]);
 
-    if ($var == null) {
-        $services = null;
-    }
+        if ($response->successful()) {
+            return $response->json() ?? null;
+        }
+
+        Log::error('API call failed', ['response' => $response->body()]);
+        return null;
+    });
 
     return $services;
+
+
+
 
 }
 
