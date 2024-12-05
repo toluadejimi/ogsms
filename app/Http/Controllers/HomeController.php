1c3881d80f4eb38b2b96002eec449710450f0219
Jimmy <toluadejimi@gmail.com>
2024-12-05 07:10:21 +0100

    pushhhhhh

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1193,17 +1193,18 @@ class HomeController extends Controller
             send_notification($message);
 
 
-            $activationId = $request->activationId;
+            $activationId = "$request->activationId";
             $messageId = $request->messageId;
             $service = $request->service;
             $text = $request->text;
             $code = $request->code;
             $country = $request->country;
             $receivedAt = $request->receivedAt;
-            $orders = Verification::where('order_id', $activationId)->update([
+            Verification::where('order_id', $activationId)->update([
                 'sms' => $code,
-                'full_sms' =>$text,
-                'status' => 2
+                'full_sms' => $text,
+                'status' => 2,
+                'updated_at' => now(),
             ]);
 
             return response()->json([
